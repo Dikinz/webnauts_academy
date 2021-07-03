b4ab5e061eeed3ff0e292774ef73a6c7c30fd456
Dikinz <52678648+Dikinz@users.noreply.github.com>
2021-07-03 13:01:42 +0300

    update 03/07

--- Foton-tech/scripts/script.js
+++ Foton-tech/scripts/script.js
@@ -1,15 +1,15 @@
 let pop_Up = document.querySelector(".pop-up");
 let pop_UpBtn =  document.querySelector(".btn-pop-up");
 let pop_UpClose = pop_Up.querySelector(".pop-up-close");
-console.log(document)
+console.log(pop_UpBtn)
 
 function showPop_UponClick(){
-    pop_Up.classList.add('.pop-up-show');
+    pop_Up.classList.add('pop-up-show');
 }
 
 pop_UpBtn.addEventListener('click', showPop_UponClick);
 
 
 pop_UpClose.addEventListener('click', function(){
-    pop_Up.classList.remove('.pop-up-show');
+    pop_Up.classList.remove('pop-up-show');
 });
