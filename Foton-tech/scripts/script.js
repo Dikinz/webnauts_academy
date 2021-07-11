2c65db983e664b789a5815a1e0ce709d84f10b9b
Dikinz <52678648+Dikinz@users.noreply.github.com>
2021-07-11 15:47:10 +0300

    update

--- Foton-tech/scripts/script.js
+++ Foton-tech/scripts/script.js
@@ -1,29 +1,32 @@
 let pop_Up = document.querySelector(".pop-up");
-let pop_UpBtn =  document.querySelector(".btn-pop-up");
-let pop_UpClose = pop_Up.querySelector(".pop-up-close");
-console.log(pop_UpBtn)
+let pop_UpBtn = document.querySelector(".btn-pop-up");
+if (pop_UpBtn) {
+  let pop_UpClose = pop_Up.querySelector(".pop-up-close");
 
-function showPop_UponClick(){
+  function showPop_UponClick() {
     pop_Up.classList.add('pop-up-show');
+  }
+
+  pop_UpBtn.addEventListener('click', showPop_UponClick);
+
+
+  pop_UpClose.addEventListener('click', function () {
+    pop_Up.classList.remove('pop-up-show');
+  });
+
 }
 
-pop_UpBtn.addEventListener('click', showPop_UponClick);
 
-
-pop_UpClose.addEventListener('click', function(){
-    pop_Up.classList.remove('pop-up-show');
+let swiper = new Swiper(".my_Swiper", {
+  centeredSlides: true,
+  slidesPerView: 1.2,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
-let swiper = new Swiper(".mySwiper", {
-    centeredSlides: true,
-    slidesPerView: 1.2,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
